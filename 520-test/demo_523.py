0ab564fbad67a0cb0cc2da93b94221e6f01094e9
xiaoxiao yan <76906208+yanxiao333@users.noreply.github.com>
2023-06-01 16:24:09 +0800

    my_study_demo_6_1

--- 520-test/demo_523.py
+++ 520-test/demo_523.py
@@ -1,4 +1,3 @@
-#!C:/Users/凡人/AppData/Local/Programs/Python/Python39/Python.exe
 # -*- coding:UTF-8 -*-
 # AUTHOR: XiaoDong Yan
 # FILE: E:\vscode_items\python-js-study-two\520-test\demo_523.py
@@ -11,10 +10,61 @@ from copyheaders import headers_raw_to_dict
 
 session = HTMLSession()
 str = '''
-
+Accept:
+application/json, text/plain, */*
+Accept-Encoding:
+gzip, deflate, br
+Accept-Language:
+zh-CN,zh;q=0.9
+Referer:
+https://www.xingtu.cn/creative/visitor/market/index
+Sec-Ch-Ua:
+"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"
+Sec-Ch-Ua-Mobile:
+?0
+Sec-Ch-Ua-Platform:
+"Windows"
+Sec-Fetch-Dest:
+empty
+Sec-Fetch-Mode:
+cors
+Sec-Fetch-Site:
+same-origin
+User-Agent:
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36
+# X-Csrftoken:
+# FIerUNA15Wao2k7qjGtlz9ZbXwrrMHOu
+# X-Login-Source:
+# 1
+# X-Secsdk-Csrf-Token:
+# 000100000001f82c8cc8cde26aace4f757f115ca173e4645a4aee283b20f0b570bafebe7937517647989bb008550
 '''
-url = ''
+url = 'https://www.xingtu.cn/gw/api/search/spu'
 headers = headers_raw_to_dict(str)
+data = {
+    "keyword": "",
+    "order_by": "score",
+    "sort_type": 2,
+    "page": 1,
+    "limit": 30,
+    "filters": {
+        "first_category_id": "200",
+        "second_category_id": "20001",
+        "industry_id": "1903",
+        "has_material": 0,
+        "project": 0
+    }
+}
+
+
+def get_data_one():
+    try:
+        res = session.post(url=url, headers=headers, data=data)
+        if res.status_code == 200:
+            print(res.text)
+
+    except Exception as e:
+        print("Error: " + str(e))
 
 
 def main():
